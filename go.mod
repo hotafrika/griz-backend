17064c3e26b5b78474222fa56deb8d07f2d623b8
Arthur <seregovaa@gmail.com>
2021-11-08 00:42:05 +0300

    add jwt generation

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/EDDYCJY/fake-useragent v0.2.0
 	github.com/PuerkitoBio/goquery v1.7.1
 	github.com/go-resty/resty/v2 v2.6.0
+	github.com/golang-jwt/jwt/v4 v4.1.0
 	github.com/makiuchi-d/gozxing v0.1.1
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
---
+++ internal/server/app/authtoken/authtoken.go
@@ -0,0 +1,38 @@
+package authtoken
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+// JWT for creating tokens
+type JWT struct {
+	Duration time.Duration
+	key      []byte
+}
+
+// NewJWT creates new JWT tokenizer
+func NewJWT(key []byte, d time.Duration) *JWT {
+	return &JWT{
+		Duration: d,
+		key:      key,
+	}
+}
+
+// NewJWTFromString creates new JWT tokenizer
+func NewJWTFromString(skey string, d time.Duration) *JWT {
+	return NewJWT([]byte(skey), d)
+}
+
+// MakeByID creates new token
+func (j JWT) MakeByID(id uint64) (string, error) {
+	atClaims := jwt.MapClaims{}
+	atClaims["id"] = id
+	atClaims["exp"] = time.Now().Add(j.Duration).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString(j.key)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
