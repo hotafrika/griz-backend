19db411cf6e1c66c0b3ee3bd245ca384f0bc3b68
Arthur <seregovaa@gmail.com>
2021-11-17 23:28:39 +0300

    add env parsing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,8 @@ import (
 	inmemory2 "github.com/hotafrika/griz-backend/internal/server/infrastructure/database/inmemory"
 	"github.com/rs/zerolog"
 	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -25,6 +27,72 @@ func main() {
 	encryptionAuthTokenString := "abc"
 	encryptionHashString := "1234567812345678" // 16symbols
 
+	// ENV parsing
+	ba, ok := os.LookupEnv("BACKEND_ADDRESS")
+	if ok {
+		bindAddr = ba
+	}
+	rt, ok := os.LookupEnv("REQUEST_TIMEOUT")
+	if ok {
+		rti, err := strconv.Atoi(rt)
+		if err == nil {
+			reqTimeout = time.Duration(rti) * time.Second
+		}
+	}
+	prt, ok := os.LookupEnv("PARSE_REQUEST_TIMEOUT")
+	if ok {
+		prti, err := strconv.Atoi(prt)
+		if err == nil {
+			parseTimeout = time.Duration(prti) * time.Second
+		}
+	}
+	ll, ok := os.LookupEnv("LOG_LEVEL")
+	if ok {
+		lli, err := strconv.Atoi(ll)
+		if err == nil {
+			if lli < -1 || lli > 6 {
+				log.Fatal("LOG_LEVEL must be between -1 and 6")
+			}
+			logLevel = zerolog.Level(lli)
+		}
+	}
+	catt, ok := os.LookupEnv("CACHE_AUTH_TOKEN_TTL")
+	if ok {
+		catti, err := strconv.Atoi(catt)
+		if err == nil {
+			authTokenTTL = time.Duration(catti) * time.Second
+		}
+	}
+	cht, ok := os.LookupEnv("CACHE_HASH_TTL")
+	if ok {
+		chti, err := strconv.Atoi(cht)
+		if err == nil {
+			hashTTL = time.Duration(chti) * time.Second
+		}
+	}
+	cslt, ok := os.LookupEnv("CACHE_SOCIAL_LINK_TTL")
+	if ok {
+		cslti, err := strconv.Atoi(cslt)
+		if err == nil {
+			socialLinkTTL = time.Duration(cslti) * time.Second
+		}
+	}
+	pek, ok := os.LookupEnv("PASSWORD_ENCRYPTION_KEY")
+	if ok {
+		encryptionPassString = pek
+	}
+	atek, ok := os.LookupEnv("AUTH_TOKEN_ENCRYPTION_KEY")
+	if ok {
+		encryptionAuthTokenString = atek
+	}
+	hek, ok := os.LookupEnv("HASH_ENCRYPTION_KEY")
+	if ok {
+		if len(hek) != 16 {
+			log.Fatal("len of HASH_ENCRYPTION_KEY is not 16 bytes")
+		}
+		encryptionAuthTokenString = hek
+	}
+
 	logger := zerolog.Logger{}
 	logger = logger.Level(logLevel)
 	cache := inmemory.NewCache()
@@ -36,6 +104,7 @@ func main() {
 	if err != nil {
 		log.Fatal("unable to initialize hash encryptor")
 	}
+
 	service := app.NewCodeService(
 		authTokenTTL,
 		hashTTL,
